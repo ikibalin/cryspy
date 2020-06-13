5ae4fc1c6050c5abad62845212d68b1e7cc5bc7f
ikibalin <yurikibalin@outlook.com>
2020-06-13 22:28:38 +0200

    method ortogonalize_matrix was added

--- cryspy/corecif/cl_cell.py
+++ cryspy/corecif/cl_cell.py
@@ -16,6 +16,7 @@ from cryspy.common.cl_fitable import Fitable
 import cryspy.corecif.CONSTANTS_AND_FUNCTIONS as CONSTANTS_AND_FUNCTIONS
 from cryspy.symcif.cl_space_group import SpaceGroup
 
+import cryspy.cif_like.CONSTANTS_AND_FUNCTIONS as CONSTANTS_AND_FUNCTIONS_cif_like
 
 
 class Cell(ItemConstr):
@@ -947,6 +948,8 @@ According to IT_C Section 1.1.2 Lattice vectors, point rows and net planes
     def closest_distance_between_fractions(self, fract_1: numpy.ndarray, fract_2: numpy.ndarray) -> numpy.ndarray:
         """
 Give closest distance between two fractions
+
+The shape of input arrays [3, n]
         """
         if len(fract_1.shape) == 1:
             fract_1_p = fract_1.reshape(fract_1.size, 1)
@@ -978,10 +981,34 @@ According to IT_C Section 1.1.2 Lattice vectors, point rows and net planes
                 2.*x*y*a*b*c_g + 2.*x*z*a*c*c_b + 2.*y*z*b*c*c_a)
         return t_sq
 
-    def report_cell(self):
+    def ortogonalize_matrix(self, l_11, l_12, l_13, l_21, l_22, l_23, l_31,
+                     l_32, l_33):
+        """
+matrix l_ij is defined in coordinate system (a, b, c)
+
+output matrix s_ij is defined in Cartezian coordinate system defined as x||a*, z||c, y= [z x] (right handed)
+        """
+        m_m_norm = self.m_m_norm
+        
+        r11, r12, r13 = m_m_norm[0, 0], m_m_norm[0, 1], m_m_norm[0, 2]
+        r21, r22, r23 = m_m_norm[1, 0], m_m_norm[1, 1], m_m_norm[1, 2]
+        r31, r32, r33 = m_m_norm[2, 0], m_m_norm[2, 1], m_m_norm[2, 2]
+        
+        s_11, s_12, s_13, s_21, s_22, s_23, s_31, s_32, s_33 = CONSTANTS_AND_FUNCTIONS_cif_like.calc_mRmCmRT(
+                r11, r12, r13, r21, r22, r23, r31, r32, r33,
+                l_11, l_12, l_13, l_21, l_22, l_23, l_31, l_32, l_33)        
+
+        return s_11, s_12, s_13, s_21, s_22, s_23, s_31, s_32, s_33
+
+
+    def report_cell(self) -> str:
         """
 Make a report about cell object in string format
         """
+        if self.is_defined:
+            self.form_object
+        else:
+            return ""
         ls_out = []
         ls_out.append("Unit cell (Ang.):")
         ls_out.append(f"{float(self.length_a):9.5f} {float(self.length_b):9.5f} {float(self.length_c):9.5f}")
