ebfb49e375f0810f8a1ddc25ac78c752bb0b82a1
Iurii KIBALIN <yurikibalin@outlook.com>
2020-09-15 18:37:45 +0200

    version 0.4.5

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ with open(f_name, 'r') as f:
 
 setup(
     name='cryspy',
-    version='0.4.4',
+    version='0.4.5',
     description='PNPD data analysis',
     long_description = long_description,
     author='Iurii Kibalin',
