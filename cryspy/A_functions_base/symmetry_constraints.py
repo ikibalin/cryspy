fix bug with sc_chi and increase precision for atom_position

cryspy/A_functions_base/symmetry_constraints.py
@@ -1,7 +1,7 @@
 import numpy
 
 from cryspy.A_functions_base.unit_cell import calc_m_m_by_unit_cell_parameters
-from cryspy.A_functions_base.matrix_operations import calc_m1_m2_inv_m1, calc_m_v
+from cryspy.A_functions_base.matrix_operations import calc_m1_m2_inv_m1, calc_m_v, calc_mm_as_m_q_inv_m, calc_mm_as_m1_m2_inv_m1
 
 def calc_sc_fract_sc_b(symm_elems, atom_fract_xyz):
     sc_fract = (symm_elems[4:13]).sum(axis=1)/symm_elems.shape[1]
@@ -35,7 +35,7 @@ def calc_sc_beta(symm_elems):
     return sc_beta
 
 
-def calc_sc_chi(symm_elems, unit_cell_parameters, flag_unit_cell_parameters:bool=False):
+def calc_sc_chi(symm_elems, unit_cell_parameters, flag_unit_cell_parameters: bool = False):
     """Calculate symmetry constraint matrix for susceptibility
     It is supposed that determinant of r matrix is 1
     """
@@ -46,30 +46,8 @@ def calc_sc_chi(symm_elems, unit_cell_parameters, flag_unit_cell_parameters:bool
                     
     r_ccs, dder_r_ccs = calc_m1_m2_inv_m1(m_m, r_direct, flag_m1=False, flag_m2=False)
 
-    r_11, r_12, r_13 = r_ccs[0], r_ccs[1], r_ccs[2]
-    r_21, r_22, r_23 = r_ccs[3], r_ccs[4], r_ccs[5]
-    r_31, r_32, r_33 = r_ccs[6], r_ccs[7], r_ccs[8]
-    
-    det_r = numpy.expand_dims(numpy.expand_dims(
-        r_11*r_22*r_33 - r_11*r_23*r_32  - r_12*r_21*r_33 + r_12*r_23*r_31  + r_13*r_21*r_32 - r_13*r_22*r_31,
-        axis=0), axis=0)
-    m_mn = numpy.array([
-        [r_11*(r_22*r_33 - r_23*r_32), r_12*(-r_21*r_33 + r_23*r_31), r_13*(r_21*r_32 - r_22*r_31), r_11*(-r_21*r_33 + r_23*r_31) + r_12*(r_22*r_33 - r_23*r_32), r_11*(r_21*r_32 - r_22*r_31) + r_13*(r_22*r_33 - r_23*r_32), r_12*(r_21*r_32 - r_22*r_31) + r_13*(-r_21*r_33 + r_23*r_31)],
-        [r_11*(-r_12*r_33 + r_13*r_32), r_12*(r_11*r_33 - r_13*r_31), r_13*(-r_11*r_32 + r_12*r_31), r_11*(r_11*r_33 - r_13*r_31) + r_12*(-r_12*r_33 + r_13*r_32), r_11*(-r_11*r_32 + r_12*r_31) + r_13*(-r_12*r_33 + r_13*r_32), r_12*(-r_11*r_32 + r_12*r_31) + r_13*(r_11*r_33 - r_13*r_31)],
-        [r_11*(r_12*r_23 - r_13*r_22), r_12*(-r_11*r_23 + r_13*r_21), r_13*(r_11*r_22 - r_12*r_21), r_11*(-r_11*r_23 + r_13*r_21) + r_12*(r_12*r_23 - r_13*r_22), r_11*(r_11*r_22 - r_12*r_21) + r_13*(r_12*r_23 - r_13*r_22), r_12*(r_11*r_22 - r_12*r_21) + r_13*(-r_11*r_23 + r_13*r_21)],
-        [r_21*(r_22*r_33 - r_23*r_32), r_22*(-r_21*r_33 + r_23*r_31), r_23*(r_21*r_32 - r_22*r_31), r_21*(-r_21*r_33 + r_23*r_31) + r_22*(r_22*r_33 - r_23*r_32), r_21*(r_21*r_32 - r_22*r_31) + r_23*(r_22*r_33 - r_23*r_32), r_22*(r_21*r_32 - r_22*r_31) + r_23*(-r_21*r_33 + r_23*r_31)],
-        [r_21*(-r_12*r_33 + r_13*r_32), r_22*(r_11*r_33 - r_13*r_31), r_23*(-r_11*r_32 + r_12*r_31), r_21*(r_11*r_33 - r_13*r_31) + r_22*(-r_12*r_33 + r_13*r_32), r_21*(-r_11*r_32 + r_12*r_31) + r_23*(-r_12*r_33 + r_13*r_32), r_22*(-r_11*r_32 + r_12*r_31) + r_23*(r_11*r_33 - r_13*r_31)],
-        [r_21*(r_12*r_23 - r_13*r_22), r_22*(-r_11*r_23 + r_13*r_21), r_23*(r_11*r_22 - r_12*r_21), r_21*(-r_11*r_23 + r_13*r_21) + r_22*(r_12*r_23 - r_13*r_22), r_21*(r_11*r_22 - r_12*r_21) + r_23*(r_12*r_23 - r_13*r_22), r_22*(r_11*r_22 - r_12*r_21) + r_23*(-r_11*r_23 + r_13*r_21)],
-        [r_31*(r_22*r_33 - r_23*r_32), r_32*(-r_21*r_33 + r_23*r_31), r_33*(r_21*r_32 - r_22*r_31), r_31*(-r_21*r_33 + r_23*r_31) + r_32*(r_22*r_33 - r_23*r_32), r_31*(r_21*r_32 - r_22*r_31) + r_33*(r_22*r_33 - r_23*r_32), r_32*(r_21*r_32 - r_22*r_31) + r_33*(-r_21*r_33 + r_23*r_31)],
-        [r_31*(-r_12*r_33 + r_13*r_32), r_32*(r_11*r_33 - r_13*r_31), r_33*(-r_11*r_32 + r_12*r_31), r_31*(r_11*r_33 - r_13*r_31) + r_32*(-r_12*r_33 + r_13*r_32), r_31*(-r_11*r_32 + r_12*r_31) + r_33*(-r_12*r_33 + r_13*r_32), r_32*(-r_11*r_32 + r_12*r_31) + r_33*(r_11*r_33 - r_13*r_31)],
-        [r_31*(r_12*r_23 - r_13*r_22), r_32*(-r_11*r_23 + r_13*r_21), r_33*(r_11*r_22 - r_12*r_21), r_31*(-r_11*r_23 + r_13*r_21) + r_32*(r_12*r_23 - r_13*r_22), r_31*(r_11*r_22 - r_12*r_21) + r_33*(r_12*r_23 - r_13*r_22), r_32*(r_11*r_22 - r_12*r_21) + r_33*(-r_11*r_23 + r_13*r_21)]],
-        dtype=r_ccs.dtype)
+    res, dder_mm = calc_mm_as_m_q_inv_m(r_ccs, flag_m= flag_unit_cell_parameters)
 
-    flag = numpy.all(numpy.isclose(numpy.abs(det_r),1))
-    if flag:
-        res = m_mn*det_r
-    else:
-        res = m_mn/det_r
     mm = res.sum(axis=2)/res.shape[2]
 
     sc_chi = numpy.stack([mm[0,:], mm[4,:], mm[8,:], mm[1,:], mm[2,:], mm[5,:]], axis=0)
@@ -88,30 +66,8 @@ def calc_sc_chi_full(symm_elems, unit_cell_parameters, flag_unit_cell_parameters
                     
     r_ccs, dder_r_ccs = calc_m1_m2_inv_m1(m_m, r_direct, flag_m1=False, flag_m2=False)
 
-    r_11, r_12, r_13 = r_ccs[0], r_ccs[1], r_ccs[2]
-    r_21, r_22, r_23 = r_ccs[3], r_ccs[4], r_ccs[5]
-    r_31, r_32, r_33 = r_ccs[6], r_ccs[7], r_ccs[8]
-    det_r = numpy.expand_dims(numpy.expand_dims(
-        r_11*r_22*r_33 - r_11*r_23*r_32  - r_12*r_21*r_33 + r_12*r_23*r_31  + r_13*r_21*r_32 - r_13*r_22*r_31,
-        axis=0), axis=0)
-    
-    m_mm = numpy.array([
-        [r_11*(r_22*r_33 - r_23*r_32), r_11*(-r_21*r_33 + r_23*r_31), r_11*(r_21*r_32 - r_22*r_31), r_12*(r_22*r_33 - r_23*r_32), r_12*(-r_21*r_33 + r_23*r_31), r_12*(r_21*r_32 - r_22*r_31), r_13*(r_22*r_33 - r_23*r_32), r_13*(-r_21*r_33 + r_23*r_31), r_13*(r_21*r_32 - r_22*r_31)],
-        [r_11*(-r_12*r_33 + r_13*r_32), r_11*(r_11*r_33 - r_13*r_31), r_11*(-r_11*r_32 + r_12*r_31), r_12*(-r_12*r_33 + r_13*r_32), r_12*(r_11*r_33 - r_13*r_31), r_12*(-r_11*r_32 + r_12*r_31), r_13*(-r_12*r_33 + r_13*r_32), r_13*(r_11*r_33 - r_13*r_31), r_13*(-r_11*r_32 + r_12*r_31)],
-        [r_11*(r_12*r_23 - r_13*r_22), r_11*(-r_11*r_23 + r_13*r_21), r_11*(r_11*r_22 - r_12*r_21), r_12*(r_12*r_23 - r_13*r_22), r_12*(-r_11*r_23 + r_13*r_21), r_12*(r_11*r_22 - r_12*r_21), r_13*(r_12*r_23 - r_13*r_22), r_13*(-r_11*r_23 + r_13*r_21), r_13*(r_11*r_22 - r_12*r_21)],
-        [r_21*(r_22*r_33 - r_23*r_32), r_21*(-r_21*r_33 + r_23*r_31), r_21*(r_21*r_32 - r_22*r_31), r_22*(r_22*r_33 - r_23*r_32), r_22*(-r_21*r_33 + r_23*r_31), r_22*(r_21*r_32 - r_22*r_31), r_23*(r_22*r_33 - r_23*r_32), r_23*(-r_21*r_33 + r_23*r_31), r_23*(r_21*r_32 - r_22*r_31)],
-        [r_21*(-r_12*r_33 + r_13*r_32), r_21*(r_11*r_33 - r_13*r_31), r_21*(-r_11*r_32 + r_12*r_31), r_22*(-r_12*r_33 + r_13*r_32), r_22*(r_11*r_33 - r_13*r_31), r_22*(-r_11*r_32 + r_12*r_31), r_23*(-r_12*r_33 + r_13*r_32), r_23*(r_11*r_33 - r_13*r_31), r_23*(-r_11*r_32 + r_12*r_31)],
-        [r_21*(r_12*r_23 - r_13*r_22), r_21*(-r_11*r_23 + r_13*r_21), r_21*(r_11*r_22 - r_12*r_21), r_22*(r_12*r_23 - r_13*r_22), r_22*(-r_11*r_23 + r_13*r_21), r_22*(r_11*r_22 - r_12*r_21), r_23*(r_12*r_23 - r_13*r_22), r_23*(-r_11*r_23 + r_13*r_21), r_23*(r_11*r_22 - r_12*r_21)],
-        [r_31*(r_22*r_33 - r_23*r_32), r_31*(-r_21*r_33 + r_23*r_31), r_31*(r_21*r_32 - r_22*r_31), r_32*(r_22*r_33 - r_23*r_32), r_32*(-r_21*r_33 + r_23*r_31), r_32*(r_21*r_32 - r_22*r_31), r_33*(r_22*r_33 - r_23*r_32), r_33*(-r_21*r_33 + r_23*r_31), r_33*(r_21*r_32 - r_22*r_31)],
-        [r_31*(-r_12*r_33 + r_13*r_32), r_31*(r_11*r_33 - r_13*r_31), r_31*(-r_11*r_32 + r_12*r_31), r_32*(-r_12*r_33 + r_13*r_32), r_32*(r_11*r_33 - r_13*r_31), r_32*(-r_11*r_32 + r_12*r_31), r_33*(-r_12*r_33 + r_13*r_32), r_33*(r_11*r_33 - r_13*r_31), r_33*(-r_11*r_32 + r_12*r_31)],
-        [r_31*(r_12*r_23 - r_13*r_22), r_31*(-r_11*r_23 + r_13*r_21), r_31*(r_11*r_22 - r_12*r_21), r_32*(r_12*r_23 - r_13*r_22), r_32*(-r_11*r_23 + r_13*r_21), r_32*(r_11*r_22 - r_12*r_21), r_33*(r_12*r_23 - r_13*r_22), r_33*(-r_11*r_23 + r_13*r_21), r_33*(r_11*r_22 - r_12*r_21)]],
-        dtype=r_ccs.dtype)
+    res, dder_mm = calc_mm_as_m1_m2_inv_m1(r_ccs, flag_m1= flag_unit_cell_parameters)
 
-    flag = numpy.all(numpy.isclose(numpy.abs(det_r),1))
-    if flag:
-        res = m_mm*det_r
-    else:
-        res = m_mm/det_r
     sc_chi_full = res.sum(axis=2)/res.shape[2]
     dder_sc_chi_full = {}
     return sc_chi_full, dder_sc_chi_full