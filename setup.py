44e65e494743131513b372665ede7a4b9547c787
ikibalin <kibalin@ill.fr>
2024-10-28 12:13:39 +0100

    preparation pour merging

--- setup.py
+++ setup.py
@@ -58,6 +58,8 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Operating System :: OS Independent",
     ],    
     include_package_data=True,
