0a03c54e66a8e21a95ed781caf8c613d3d935eab
Iurii Kibalin <yurikibalin@outlook.com>
2026-05-22 18:20:15 +0200

    Test for SyCos and SySin

---
+++ tests/functional/sycos_sysin/test_lab6.py
@@ -0,0 +1,11 @@
+import os
+import numpy
+import cryspy
+
+
+def test_lab6():
+    dir = os.path.dirname(__file__)
+    f_name = os.path.join(dir, "LaB6.rcif")
+    rhochi = cryspy.file_to_globaln(f_name)
+    d_out = cryspy.rhochi_rietveld_refinement(rhochi)
+    assert d_out["chi_sq"] <= 261400
